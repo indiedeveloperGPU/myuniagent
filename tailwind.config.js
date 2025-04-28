7861cb67e6c67ee867a091774f020a93386525e2
indiedeveloperGPU <carducciludovicopescara@gmail.com>
2025-04-28 17:09:33 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 // tailwind.config.js
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadein: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadein: "fadein 0.8s ease-out forwards",
+      },
     },
-    plugins: [require('@tailwindcss/typography')],
-  }
-  
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+
