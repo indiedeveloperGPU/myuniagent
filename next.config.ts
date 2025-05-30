5b087c0aba47e957eb0b5c2b7611f5cc2101b4fd
indiedeveloperGPU <carducciludovicopescara@gmail.com>
2025-05-30 22:44:27 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   api: {
-    bodyParser: false, // serve per Stripe Webhook, non per checkout
+    bodyParser: {
+      sizeLimit: "1mb",
+    },
   },
 };
 
 export default nextConfig;
+
+
